8fa2feaea1d9cf91978895144d8a9fef64e21432
James Almeida <James Almeida>
2020-10-16 09:46:23 -0300

    Include partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def br_date(us_date)
     us_date.strftime('%d/%m/%Y')
   end
+
+  def rails_env
+    if Rails.env.development?
+      'Desenvolvimento'
+    elsif Rails.env.production?
+      'Produção'
+    else
+      'Teste'
+    end
+  end
 end
