6a06ae69245f2dfccdf840c6863077900ab4fed2
James Almeida <James Almeida>
2020-10-20 21:03:21 -0300

    Avoid db:seed to duplicate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cadastrando moedas..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando moedas")
+spinner.auto_spin
 
-Coin.create!(
-  description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image: 'https://img2.gratispng.com/20180604/zya/kisspng-bitcoin-com-cryptocurrency-logo-zazzle-kibuba-btc-5b15aa1f157d09.468430171528146463088.jpg'
-)
+coins = [
+  {
+    description: 'Bitcoin',
+    acronym: 'BTC',
+    url_image: 'https://img2.gratispng.com/20180604/zya/kisspng-bitcoin-com-cryptocurrency-logo-zazzle-kibuba-btc-5b15aa1f157d09.468430171528146463088.jpg'
+  },
 
-Coin.create!(
-  description: 'Etherium',
-  acronym: 'ETH',
-  url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/768px-ETHEREUM-YOUTUBE-PROFILE-PIC.png'
-)
+  {
+    description: 'Etherium',
+    acronym: 'ETH',
+    url_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png/768px-ETHEREUM-YOUTUBE-PROFILE-PIC.png'
+  },
 
-Coin.create!(
-  description: 'Dash',
-  acronym: 'DASH',
-  url_image: 'https://cryptomined.com/images/flags/dash_circle_Flag_1.png'
-)
+  {
+    description: 'Dash',
+    acronym: 'DASH',
+    url_image: 'https://cryptomined.com/images/flags/dash_circle_Flag_1.png'
+  }
+]
 
-puts "Moedas cadastradas com sucesso"
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+
+spinner.success
